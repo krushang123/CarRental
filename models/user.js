7d8f9660b7e4b9f87bbd7de179bb3236b78c5b3d
krushang123 <patelkrushang2010@gmail.com>
2020-05-03 10:43:34 +0530

    updated user schema for email and password

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, require: true},
+    email: {type: String, required: true},
+    password:{type: String, required: true, select: false},
+    name: {type: String, require: true },
     contactNumber: {type: Number, require: true}
     
 });
+
+
+UserSchema.methods.encryptPassword = async password => {
+    const salt = await bcrypt.genSalt(5);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+};
+
+UserSchema.methods.validPassword = async function(candidatePassword) {
+    const result = await bcrypt.compare(candidatePassword, this.password );
+    return result;
+};
+
 
 const User = mongoose.model('User', userSchema);
 
